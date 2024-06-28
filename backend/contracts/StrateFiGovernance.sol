bcff83c5c2be9afad3146dc761068fc0333e2ecb
Tony-S201 <swierz.tony@gmail.com>
2024-06-28 11:28:49 +0200

    feat: install openzeppelin contract package, put natspec to contracts

--- backend/contracts/StrateFiGovernance.sol
+++ backend/contracts/StrateFiGovernance.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/governance/Governor.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
@@ -8,16 +8,28 @@ import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 
+/**
+ * @title Governance contract for StrateFi DAO.
+ * @dev Extends various OpenZeppelin governance modules.
+ */
 contract StrateFiGovernance is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
+    /**
+     * @dev Initialize the contract.
+     * @param _token The address of the governance token used for voting.
+     */
     constructor(IVotes _token)
         Governor("StrateFiGovernance")
-        GovernorSettings(7 days, 2 weeks, 0)
+        GovernorSettings(0 days, 2 weeks, 0)
         GovernorVotes(_token)
         GovernorVotesQuorumFraction(66)
     {}
 
     // The following functions are overrides required by Solidity.
 
+    /**
+     * @notice Override function to custom the voting delay value.
+     * @return The voting delay required for voting.
+     */
     function votingDelay()
         public
         view
@@ -27,6 +39,10 @@ contract StrateFiGovernance is Governor, GovernorSettings, GovernorCountingSimpl
         return super.votingDelay();
     }
 
+    /**
+     * @notice Override function to custom the voting period value.
+     * @return The voting period required for voting.
+     */
     function votingPeriod()
         public
         view
@@ -36,6 +52,10 @@ contract StrateFiGovernance is Governor, GovernorSettings, GovernorCountingSimpl
         return super.votingPeriod();
     }
 
+    /**
+     * @notice Override function to custom the quorum value (min voters to accept the proposal).
+     * @return The quorum required for voting.
+     */
     function quorum(uint256 blockNumber)
         public
         view
@@ -45,6 +65,10 @@ contract StrateFiGovernance is Governor, GovernorSettings, GovernorCountingSimpl
         return super.quorum(blockNumber);
     }
 
+    /**
+     * @notice Override function to custom the proposal threshold value.
+     * @return The min of votes required to create a proposal.
+     */
     function proposalThreshold()
         public
         view
@@ -52,5 +76,46 @@ contract StrateFiGovernance is Governor, GovernorSettings, GovernorCountingSimpl
         returns (uint256)
     {
         return super.proposalThreshold();
+    }
+
+    /**
+     * @notice Override function to create a proposal with default parameters except for the description.
+     * @param targets List of contract addresses.
+     * @param values List of ETH values to send.
+     * @param calldatas List of function to call.
+     * @param description Description of the proposal.
+     */
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    )
+        public
+        override(Governor)
+        returns (uint256)
+    {
+        address[] memory targets = new address[](1);
+        targets[0] = 0x0000000000000000000000000000000000000000;
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = "";
+
+        address proposer = _msgSender();
+
+        // check description restriction
+        if (!_isValidDescriptionForProposer(proposer, description)) {
+            revert GovernorRestrictedProposer(proposer);
+        }
+
+        // check proposal threshold
+        uint256 proposerVotes = getVotes(proposer, clock() - 1);
+        uint256 votesThreshold = proposalThreshold();
+        if (proposerVotes < votesThreshold) {
+            revert GovernorInsufficientProposerVotes(proposer, proposerVotes, votesThreshold);
+        }
+
+        return _propose(targets, values, calldatas, description, proposer);
     }
 }
--- backend/contracts/StrateFiToken.sol
+++ backend/contracts/StrateFiToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -9,38 +9,65 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
+/**
+ * @title Token contract for StrateFi.
+ * @dev Extends various OpenZeppelin token modules for the SFT (StrateFiToken) token.
+ */
 contract StrateFiToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes {
+    /**
+     * @notice Initialize the contract and mint 1000000000 tokens.
+     * @param initialOwner The address of the owner of the contract.
+     */
     constructor(address initialOwner)
         ERC20("StrateFiToken", "SFT")
         Ownable(initialOwner)
         ERC20Permit("StrateFiToken")
     {
-        _mint(msg.sender, 100000000 * 10 ** decimals());
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 
+    /**
+     * @notice Pause all token transfers.
+     * @dev Only callable by the owner of the contract.
+     */
     function pause() public onlyOwner {
         _pause();
     }
 
+    /**
+     * @notice Unpause all token transfers.
+     * @dev Only callable by the owner of the contract.
+     */
     function unpause() public onlyOwner {
         _unpause();
     }
 
+    /**
+     * @notice Mint new tokens and send them to a recipient.
+     * @dev Only callable by the owner of the contract.
+     * @param to The address which mint the tokens.
+     * @param amount Amount of tokens to mint.
+     */
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function clock() public view override returns (uint48) {
-        return uint48(block.timestamp);
-    }
+    // The following functions are overrides required by Solidity.
 
-    // solhint-disable-next-line func-name-mixedcase
+    /**
+     * @notice Override function to custom clock mode type to timestamp instead of block.
+     * @return The clock mode of this token contract.
+     */
     function CLOCK_MODE() public pure override returns (string memory) {
         return "mode=timestamp";
     }
 
-    // The following functions are overrides required by Solidity.
-
+    /**
+     * @dev Internal function to update token balances and perform voting snapshot.
+     * @param from The address from which tokens are transferred.
+     * @param to The address to which tokens are transferred.
+     * @param value The amount of tokens transferred.
+     */
     function _update(address from, address to, uint256 value)
         internal
         override(ERC20, ERC20Pausable, ERC20Votes)
@@ -48,6 +75,11 @@ contract StrateFiToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Per
         super._update(from, to, value);
     }
 
+    /**
+     * @dev Returns the nonce for the given owner, required by ERC20Permit.
+     * @param owner The address for which to return the nonce.
+     * @return The nonce of the owner.
+     */
     function nonces(address owner)
         public
         view
