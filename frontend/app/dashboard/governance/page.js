4f107f6dfdc7b3b981348b8435cc42d961e3f473
Tony-S201 <swierz.tony@gmail.com>
2024-06-29 21:59:29 +0200

    feat: user can create a proposal and display it

--- frontend/app/dashboard/governance/page.js
+++ frontend/app/dashboard/governance/page.js
@@ -1,18 +1,25 @@
 'use client';
 
+// Constants
 import { contractAbi, contractAddress } from "@/constants/governance";
-import { useReadContract, useWriteContract, useWaitForTransactionReceipt, useWatchContractEvent } from "wagmi";
+
+// Wagmi
+import { useReadContract, useWatchContractEvent, useWriteContract } from "wagmi";
+
+// React
 import { useState } from "react";
+
+// UI
 import { Button } from "@/components/ui/button";
 
 const Governance = () => {
 
-  const [proposals, setProposals] = useState();
+  const [ proposals, setProposals ] = useState();
 
   useWatchContractEvent({
     address: contractAddress,
     abi: contractAbi,
-    eventName: "",
+    eventName: "ProposalCreated",
     onLogs(logs) {
       console.log(logs)
       if (logs.length > 0) {
@@ -21,20 +28,40 @@ const Governance = () => {
     }
   })
 
-  const propose = () => {
-    console.log('propose')
+  const { writeContractAsync } = useWriteContract();
+
+  const handleAddProposal = async() => {
+    try {
+      const { data: result, onSuccess: isSuccess } = await writeContractAsync({
+        abi: contractAbi,
+        address: contractAddress,
+        functionName: "propose",
+        args: [
+          ["0x0000000000000000000000000000000000000000"],
+          [0],
+          ["0x"],
+          "hello, it's the second proposal of the stratefi dapp !"
+        ]
+      })
+    } catch (err) {
+      console.log(err)
+    }
+    
   }
 
   return (
     <div>
-      <Button onClick={() => propose()}>PROPOSE</Button>
+      <Button onClick={handleAddProposal}>PROPOSE</Button>
       {!proposals || proposals.length === 0 ? (
         <div>Aucune proposition</div>
       ) : (
         <ul>
           {proposals.map((proposal, index) => (
             <li key={index}>
-              <div>{proposal.name}</div>
+              <div>{Number(proposal.args.proposalId)}</div>
+              <div>By: {proposal.args.proposer}</div>
+              <div>{proposal.args.description}</div>
+              <div>{Number(proposal.args.voteEnd)}</div>
             </li>
           ))}
           <li></li>
