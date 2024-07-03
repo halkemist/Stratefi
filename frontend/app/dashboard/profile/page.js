6b3ed720cfd3f990856a1b4dd5b7b9da3025164f
Tony-S201 <swierz.tony@gmail.com>
2024-07-03 15:53:01 +0200

    feat: profile page to see wallet balances

--- frontend/app/dashboard/profile/page.js
+++ frontend/app/dashboard/profile/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 // UI
 import { FaWallet } from "react-icons/fa";
 import {
@@ -7,35 +9,109 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
+import { FaSpinner } from "react-icons/fa";
+
+// React & Wagmi & Viem & Config
+import { config } from "@/app/config";
+import { getBalance } from "viem/actions";
+import { useAccount } from "wagmi";
+import { useEffect, useState } from "react";
+import { formatEther } from "viem";
+
+// Constants
+import { contractAbi as SFTabi, contractAddress as SFTaddress } from "@/constants/token";
 
 const Profile = () => {
+
+  const { address: userAddress } = useAccount();
+
+  const [balances, setBalances] = useState({
+    eth: 0,
+    sft: 0
+  });
+
+  const [loader, setLoader] = useState(true);
+
+  const balanceETH = async() => {
+    getBalance(config, {
+      address: userAddress,
+    }).then((response) => {
+      const ethBalance = formatEther(response)
+      if (ethBalance > 0) {
+        setBalances((prevBalances) => ({
+          ...prevBalances,
+          eth: ethBalance
+        }));
+      }
+    });
+  };
+
+  const balanceSFT = async() => {
+    config.readContract({
+      abi: SFTabi,
+      address: SFTaddress,
+      functionName: 'balanceOf',
+      args: [
+        userAddress
+      ]
+    }).then((response) => {
+      const sftBalance = formatEther(response)
+      if (sftBalance > 0) {
+        setBalances((prevBalances) => ({
+          ...prevBalances,
+          sft: sftBalance
+        }));
+      }
+    });
+  };
+
+  useEffect(() => {
+    const fetchBalances = async() => {
+      await balanceETH();
+      await balanceSFT();
+      setLoader(false);
+    }
+
+    if (userAddress) {
+      fetchBalances();
+    }
+  }, [userAddress]);
+
   return (
     <>
       <div className="flex items-center gap-2 mb-2">
         <FaWallet/>
         <h2>Wallet</h2>
       </div>
-      <div className="w-2/3">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Token</TableHead>
-              <TableHead>Price</TableHead>
-              <TableHead>Amount</TableHead>
-              <TableHead className="text-right">USD Value</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">ETH</TableCell>
-              <TableCell>3,500</TableCell>
-              <TableCell>Credit Card</TableCell>
-              <TableCell className="text-right">$250.00</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
+      {loader ? (<FaSpinner className="spinner"/>) : (
+        <div className="w-2/3">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Token</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Amount</TableHead>
+                <TableHead className="text-right">USD Value</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">ETH</TableCell>
+                <TableCell>$0</TableCell>
+                <TableCell>{balances.eth}</TableCell>
+                <TableCell className="text-right">$0</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">SFT</TableCell>
+                <TableCell>$0</TableCell>
+                <TableCell>{balances.sft}</TableCell>
+                <TableCell className="text-right">$0</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      )}
     </>
   )
 }
