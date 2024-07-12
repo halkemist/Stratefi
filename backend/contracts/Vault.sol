685656ed89feca6e1e6a3abfdca66b87831de443
Tony-S201 <swierz.tony@gmail.com>
2024-07-12 12:46:08 +0200

    try to supply in aave

--- backend/contracts/Vault.sol
+++ backend/contracts/Vault.sol
@@ -2,24 +2,21 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { IPool } from "@aave/core-v3/contracts/interfaces/IPool.sol";
 
 interface IWETH {
     function deposit() external payable;
     function withdraw(uint256 amount) external;
     function approve(address guy, uint wad) external returns (bool);
-}
-
-interface IATOKEN {
-    function scaledBalanceOf(address user) external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
 }
 
 contract Vault is ReentrancyGuard {
-
     address public constant WETH_ADDRESS_BASE_SEPOLIA = 0x4200000000000000000000000000000000000006;
     address public constant A_TOKEN_WETH_BASE_SEPOLIA = 0x96e32dE4B1d1617B8c2AE13a88B9cC287239b13f;
     IWETH public immutable weth;
-    IATOKEN public immutable atoken;
-    address public immutable protocol;
+    address public pool;
+    IPool internal lendingPool;
 
     struct Account {
         uint256 balance;
@@ -29,16 +26,16 @@ contract Vault is ReentrancyGuard {
 
     mapping(address => Account) balances;
 
-    event VaultDeposited(address indexed account, uint256 amount, address protocol);
-    event VaultWithdrawed(address indexed account, uint256 amount, address protocol);
-    event ProtocolDeposited(address indexed account, uint256 amount, address protocol);
-    event ProtocolWithdrawed(address indexed account, uint256 amount, address protocol);
+    event VaultDeposited(address indexed account, uint256 amount);
+    event VaultWithdrawed(address indexed account, uint256 amount);
+    event ProtocolDeposited(address indexed account, uint256 amount);
+    event ProtocolWithdrawed(address indexed account, uint256 amount);
 
     constructor(address newProtocol) {
         require(newProtocol != address(0), "Protocol address cannot be zero");
         weth = IWETH(WETH_ADDRESS_BASE_SEPOLIA);
-        atoken = IATOKEN(A_TOKEN_WETH_BASE_SEPOLIA);
-        protocol = newProtocol;
+        pool = newProtocol;
+        lendingPool = IPool(newProtocol);
     }
 
     receive() external payable {}
@@ -47,7 +44,7 @@ contract Vault is ReentrancyGuard {
         require(msg.value > 0, "Need more funds to deposit");
         balances[userAddress].balance += msg.value;
         balances[userAddress].lastAction = block.timestamp;
-        emit VaultDeposited(userAddress, msg.value, protocol);
+        emit VaultDeposited(userAddress, msg.value);
     }
 
     function withdrawFromVault(uint256 amount) external nonReentrant {
@@ -61,7 +58,7 @@ contract Vault is ReentrancyGuard {
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "Withdraw failed");
 
-        emit VaultWithdrawed(msg.sender, amount, protocol);
+        emit VaultWithdrawed(msg.sender, amount);
     }
 
     function convertToWeth(uint256 amount) external {
@@ -81,21 +78,17 @@ contract Vault is ReentrancyGuard {
         // Update balance
         balances[msg.sender].wethBalance -= amount;
 
+        // Convert WETH to ETH
         weth.withdraw(amount);
 
-        // Convert WETH to ETH
-        //(bool success, bytes memory data) = WETH_ADDRESS_BASE_SEPOLIA.call(
-        //    abi.encodeWithSignature("withdraw(uint256)", amount)
-        //);
-
-        //require(success, "WETH to ETH conversion failed");
+        // update balance
         balances[msg.sender].balance += amount;
     }
 
     function approveProtocol(uint256 amount) external {
         // Approve WETH to interact with AAVE
         (bool success, ) = WETH_ADDRESS_BASE_SEPOLIA.call(
-            abi.encodeWithSignature("approve(address, uint256)", protocol, amount)
+            abi.encodeWithSignature("approve(address, uint256)", pool, amount)
         );
 
         require(success, "WETH approval for AAVE failed");
@@ -107,34 +100,26 @@ contract Vault is ReentrancyGuard {
         // Update balance
         balances[msg.sender].wethBalance -= amount;
 
+        // Deposit in POOL.
+        lendingPool.supply(address(weth), amount, msg.sender, 0);
+
         // Emit an event
-        emit ProtocolDeposited(msg.sender, amount, protocol);
-
-        // Deposit in AAVE
-        (bool success, ) = protocol.call(
-            abi.encodeWithSignature("supply(address, address, address, uint256, uint16)", address(weth), msg.sender, address(0), amount, 0)
-        );
-
-        require(success, "AAVE deposit failed");
+        emit ProtocolDeposited(msg.sender, amount);
     }
 
-    function withwrawFromProtocol(uint256 amount) payable external {
+    function withdrawFromProtocol(uint256 amount) payable external {
         // Update balance
         balances[msg.sender].wethBalance += amount;
 
+
+
+
         // Emit an event
-        emit ProtocolWithdrawed(msg.sender, amount, protocol);
-
-        // Withdraw from AAVE
-        (bool success, ) = protocol.call(
-            abi.encodeWithSignature("withdraw(address, address, address, uint256)", address(weth), address(this), msg.sender, amount)
-        );
-
-        require(success, "AAVE withdrawal failed");
+        emit ProtocolWithdrawed(msg.sender, amount);
     }
 
-    function getProtocolBalance(address userAddress) external view returns(uint256) {
-        return atoken.scaledBalanceOf(userAddress);
+    function getContractBalance() external view returns(uint256) {
+        return weth.balanceOf(address(this));
     }
 
     function getBalance(address userAddress) external view returns(uint256) {
