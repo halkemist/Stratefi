42067af6d39a57948fdaa374a8956503ec26524f
Tony-S201 <swierz.tony@gmail.com>
2024-07-14 22:08:56 +0200

    misssing file

---
+++ frontend/constants/atoken.js
@@ -0,0 +1,27 @@
+export const contractAddress = "0x96e32dE4B1d1617B8c2AE13a88B9cC287239b13f";
+export const contractAbi = [
+    {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "guy",
+            "type": "address"
+            },
+            {
+            "internalType": "uint256",
+            "name": "wad",
+            "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
